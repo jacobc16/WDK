1569607e78d02273e63225b398e5fac16a1122ff
Walrusking <walruskinggaming@gmail.com>
2025-09-26 20:50:52 -0400

    adds docs to cursor class

--- Helpers/Cursor.cs
+++ Helpers/Cursor.cs
@@ -13,9 +13,22 @@ namespace WDK.Helpers
 			Sell
 		}
 
+		/// <summary>
+		///     Returns true if the cursor is currently locked (in the center of the screen and invisible).
+		/// </summary>
 		public static bool IsCursorLocked => UnityEngine.Cursor.lockState == CursorLockMode.Locked;
+
+		/// <summary>
+		///     Returns true if the cursor is currently visible.
+		/// </summary>
 		public static bool IsCursorVisible => UnityEngine.Cursor.visible;
 
+		/// <summary>
+		///     Sets the cursor to the specified texture.
+		/// </summary>
+		/// <param name="texture">The texture to set the cursor to.</param>
+		/// <param name="hotspot">The hotspot of the cursor.</param>
+		/// <param name="cursorMode">The cursor mode. Default is Auto.</param>
 		public static void SetCursor(Texture2D texture, Vector2 hotspot, CursorMode cursorMode = CursorMode.Auto)
 		{
 			if (texture is null)
@@ -27,12 +40,23 @@ namespace WDK.Helpers
 			UnityEngine.Cursor.SetCursor(texture, hotspot, cursorMode);
 		}
 
+		/// <summary>
+		///     Resets the cursor to the default system cursor.
+		/// </summary>
 		public static void ResetCursor()
 		{
 			UnityEngine.Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
 		}
 
-		public static void LockCursor(bool lockCursor)
+
+		/// <summary>
+		///     Toggles the cursor lock state and visibility.
+		/// </summary>
+		/// <param name="lockCursor">
+		///     If true, locks the cursor to the center of the screen and makes it invisible. If false,
+		///     unlocks the cursor and makes it visible.
+		/// </param>
+		public static void ToggleCursor(bool lockCursor)
 		{
 			if (lockCursor)
 			{
