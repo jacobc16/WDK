87b70161c64a20c9cfa7451aeeddede8c89941d0
Walrusking <walruskinggaming@gmail.com>
2025-09-26 22:50:11 -0400

    adds TimeSince and TimeUntil

---
+++ Utility/TimeSince.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WDK.Utility
+{
+	/// <summary>
+	///     A class that tracks the time since it was created or last set.
+	/// </summary>
+	public class TimeSince
+	{
+		private readonly double _startTime;
+
+		public TimeSince()
+		{
+			_startTime = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond;
+		}
+
+		/// <summary>
+		///     The time in seconds since this instance was created or last set.
+		/// </summary>
+		public float Value
+		{
+			get => (float)( DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond - _startTime );
+			private init => _startTime = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond - value;
+		}
+
+		public static implicit operator float(TimeSince timeSince)
+		{
+			return timeSince.Value;
+		}
+
+		public static implicit operator double(TimeSince timeSince)
+		{
+			return timeSince.Value;
+		}
+
+		public static implicit operator TimeSince(float value)
+		{
+			return new TimeSince { Value = value };
+		}
+	}
+}
---
+++ Utility/TimeUntil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace WDK.Utility
+{
+	public class TimeUntil
+	{
+		public TimeUntil()
+		{
+			StartTime = DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond;
+		}
+
+		/// <summary>
+		///     The time since this instance was created or last set.
+		/// </summary>
+		public double StartTime { get; }
+
+		/// <summary>
+		///     The number of seconds we are counting to.
+		/// </summary>
+		public float Duration { get; init; }
+
+		/// <summary>
+		///     Returns true if the specified duration has passed since the creation of this TimeUntil instance or since the last
+		///     reset.
+		/// </summary>
+		public bool Passed => DateTime.Now.Ticks / (double)TimeSpan.TicksPerSecond - StartTime >= Duration;
+
+		/// <summary>
+		///     The fraction of time passed, from 0 to 1.
+		/// </summary>
+		public float Fraction => Mathf.Clamp01((float)StartTime / Duration);
+
+		public static implicit operator bool(TimeUntil timeUntil)
+		{
+			return timeUntil?.Passed ?? false;
+		}
+
+		public static implicit operator TimeUntil(float value)
+		{
+			return new TimeUntil { Duration = value };
+		}
+	}
+}
